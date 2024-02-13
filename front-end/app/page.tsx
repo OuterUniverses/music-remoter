1cd8bcf8aa28862ec2c416db687bf13fa73cfb7f
itsdapi <altgr3@foxmail.com>
2024-02-14 02:04:55 +0800

    Fix root page cannot build properly

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -2,19 +2,22 @@ import Player from "@/app/ui/player";
 import Search from "@/app/ui/search";
 import SongList from "@/app/ui/songList";
 import {checkEnv} from "@/app/lib/util";
-import {User} from "@nextui-org/react";
-import {getUserProfile} from "@/app/lib/spotify";
+import {getToken, getUserProfile} from "@/app/lib/spotify";
 import SignOutButton from "@/app/ui/signOutButton";
 import Queue from "@/app/ui/queue";
 import {Suspense} from "react";
 import QueueSkeleton from "@/app/ui/skeleton/queueSkeleton";
 import SongListSkeleton from "@/app/ui/skeleton/songListSkeleton";
+import {redirect} from "next/navigation";
+import {config} from "@/app.config";
 
+export const dynamic = 'force-dynamic'
 export default async function Page(
     {searchParams}: { searchParams?: { query?: string; page?: number; track_uri?: string;} }
 ) {
+    if (!(await getToken())) redirect(config.app.authPath)
     checkEnv(['NEXT_PUBLIC_SPOTIFY_CLIENT_ID', 'SPOTIFY_CLIENT_SECRET', 'NEXT_PUBLIC_SPOTIFY_REDIRECT_URI'])
-    const user = await getUserProfile()
+    // const user = await getUserProfile()
     const query = searchParams?.query || ''
     const currentPage = searchParams?.page || 1
 
@@ -23,9 +26,9 @@ export default async function Page(
             <div className={'flex items-center'}>
                 <h1 className={'font-bold text-2xl md:text-5xl bg-kleinblue-front text-kleinblue-back mb-3 py-3 px-5 -rotate-3 translate-x-1'}>点歌台</h1>
                 <div className={'flex-grow'}/>
-                <User name={user.display_name} avatarProps={{src: user.images[0].url}} description={user.product}
-                      className={'rounded-full bg-white px-4 py-2'}/>
-                <SignOutButton/>
+                {/*<User name={user.display_name} avatarProps={{src: user.images[0].url}} description={user.product}*/}
+                {/*      className={'rounded-full bg-white px-4 py-2'}/>*/}
+                {/*<SignOutButton/>*/}
             </div>
             <Player/>
             <Suspense fallback={<QueueSkeleton/>}>
