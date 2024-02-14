e800c5b8980b4122cf0c536b5f7051366d97ffba
itsdapi <altgr3@foxmail.com>
2024-02-14 16:25:29 +0800

    Fix auth page secret undefined

--- front-end/app/auth/page.tsx
+++ front-end/app/auth/page.tsx
@@ -1,7 +1,9 @@
 import {SpotifyLoginButton} from "@/app/ui/authButton";
 import {config} from "@/app.config";
+import { unstable_noStore as noStore } from 'next/cache';
 
 export default async function Page() {
+    noStore();
     return <div className={'h-screen w-full flex justify-center items-center bg-gray-100'}>
         <div className={'p-5 rounded-3xl bg-white/80 border-1'}>
             <SpotifyLoginButton client_id={config.api.spotifyClientID} redirect_url={config.api.spotifyRedirectUri} scope={config.api.scope}/>
